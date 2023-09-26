8413f2b6ccb730dcca90f2b651aab29208490088
jwang3421 <jwang3421@gatech.edu>
2023-09-26 01:57:13 -0400

    specify port and ip to bind to

--- application.py
+++ application.py
@@ -3,4 +3,5 @@ from website import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    # specify the port to bind to, and the IP to bind to 0.0.0.0 = bind to everything
+    application.run(host='0.0.0.0', port=5000, debug=True)
